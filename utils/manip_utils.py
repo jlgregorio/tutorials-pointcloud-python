f088f3d9512d3e1b7fb3971c71643ecf20d5baa8
Jean-Loup <jean-loup.gregorio@inetum.com>
2024-08-14 19:24:33 +0200

    continue tuto 5

--- utils/manip_utils.py
+++ utils/manip_utils.py
@@ -20,3 +20,27 @@ def rotation_zyx(theta_x=0., theta_y=0., theta_z=0.):
     ])
 
     return R_z @ R_y @ R_x
+
+
+def rotation_f_to_t(f, t):
+    """Construct a (3, 3) matrix that rotates a unit vector f into another 
+    unit vector t.
+
+    From Möller, T., & Hughes, J. F. (1999). Efficiently building a matrix to 
+    rotate one vector to another. Journal of graphics tools, 4(4), 1-4."""
+    
+    v_x, v_y, v_z = np.cross(f, t)
+    c = np.dot(f, t)
+    h = 1 / (1 + c)
+
+    # When f and t are nearly parallel the computation is numerically unstable
+    if np.linalg.norm(c) > 0.99:
+        return np.eye(3)
+
+    R = np.array([
+    [c + h*v_x**2, h*v_x*v_y - v_z, h*v_x*v_z + v_y],
+    [h*v_x*v_y + v_z, c + h*v_y**2, h*v_y*v_z - v_x],
+    [h*v_x*v_z - v_y , h*v_y*v_z + v_x, c + h*v_z**2]
+    ])
+    
+    return R
